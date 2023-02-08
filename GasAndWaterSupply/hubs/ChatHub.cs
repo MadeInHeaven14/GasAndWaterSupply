7b454820427958f30c03eaae0b42fa85b8e6ff14
artur-nikiforenko@mail.ru <artur-nikiforenko@mail.ru>
2023-02-08 21:05:41 +0300

    SignalR chat 1v1

--- GasAndWaterSupply/hubs/ChatHub.cs
+++ GasAndWaterSupply/hubs/ChatHub.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace GasAndWaterSupply.hubs
 {
     public class ChatHub : Hub
     {
-        public async Task Send(string username, string message)
+        public async Task Send(string senderName, string receiverName, string message)
         {
-            await this.Clients.All.SendAsync("Receive", username, message);
+            // получение текущего пользователя, который отправил сообщение
+            //var userName = Context.UserIdentifier;
+            //if (Context.UserIdentifier is string userName)
+            //{
+            //    await Clients.Users(to, userName).SendAsync("Receive", message, userName);
+            //}
+            await this.Clients.All.SendAsync("Receive", senderName, receiverName, message);
         }
+
+        //public override async Task OnConnectedAsync()
+        //{
+        //    await Clients.All.SendAsync("Notify", $"Приветствуем {Context.UserIdentifier}");
+        //    await base.OnConnectedAsync();
+        //}
     }
 }
